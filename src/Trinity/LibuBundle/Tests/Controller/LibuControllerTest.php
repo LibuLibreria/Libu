279cf0f3cc56446ca8b7488ca3741e9eb075fb4a
Borja <libulibreria@gmail.com>
2016-07-19 20:25:52 +0200

    Testing

--- src/Trinity/LibuBundle/Tests/Controller/LibuControllerTest.php
+++ src/Trinity/LibuBundle/Tests/Controller/LibuControllerTest.php
@@ -4,14 +4,48 @@ namespace Trinity\LibuBundle\Tests\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
 
-class DefaultControllerTest extends WebTestCase
+class LibuControllerTest extends WebTestCase
 {
     public function testIndex()
     {
+    	// Preparación
+		self::bootKernel(); 
+
+		$em = static::$kernel->getContainer()
+			->get('doctrine')
+			->getManager();
+
         $client = static::createClient();
 
-        $crawler = $client->request('GET', '/');
 
-        $this->assertContains('Hello World', $client->getResponse()->getContent());
+        /*
+        * Test /libu/caja
+        */
+        $crawlercaja = $client->request('GET', '/libu/caja');
+
+        $this->assertGreaterThan(
+            0,
+            $crawlercaja->filter('html:contains("Caja")')->count()
+        );
+
+
+        /*
+        * Test /libu/venta, con emisión de formulario incluida
+        */
+        $crawlerventa = $client->request('GET', '/libu/venta');
+
+        $form = $crawlerventa->selectButton('venta_save')->form();
+
+		$form['venta[libros3]'] = '12';
+
+		// submit the form
+		$crawlerventa = $client->submit($form);
+		$client->followRedirect();
+
+		$this->assertContains(
+		    'TOTAL: 24 euros',
+		    $client->getResponse()->getContent()
+		);
+
     }
 }
