9126d9dca074a7015ac8d2364618f0909cdba048
Borja <libulibreria@gmail.com>
2016-07-26 02:12:57 +0200

    Funciona importe con jquery

--- src/Trinity/LibuBundle/Resources/public/js/scripts.js
+++ src/Trinity/LibuBundle/Resources/public/js/scripts.js
@@ -4,13 +4,17 @@ jQuery(document).ready(function(){
         // Stop acting like a button
         e.preventDefault();
         // Get the field name
-        fieldName = $(this).attr('field');
+        var fieldName = $(this).attr('field');
+        var precio = $(this).val();
         // Get its current value
         var currentVal = parseInt($('input[id=venta_'+fieldName+']').val());
+        var totalVal = parseFloat(($('#importe')).html());    
         // If is not undefined
         if (!isNaN(currentVal)) {
             // Increment
             $('input[id=venta_'+fieldName+']').val(currentVal + 1);
+            $('#importe').html(totalVal + parseFloat(precio));
+
         } else {
             // Otherwise put a 0 there
             $('input[id=venta_'+fieldName+']').val(0);
@@ -24,10 +28,15 @@ jQuery(document).ready(function(){
         fieldName = $(this).attr('field');
         // Get its current value
         var currentVal = parseInt($('input[id=venta_'+fieldName+']').val());
+        var totalVal = parseFloat(($('#importe')).html());  
+        var precio = {'libros3': '3', 'libros1':'1'};    
+        
         // If it isn't undefined or its greater than 0
         if (!isNaN(currentVal) && currentVal > 0) {
             // Decrement one
             $('input[id=venta_'+fieldName+']').val(currentVal - 1);
+            $('#importe').html(totalVal - parseFloat(precio[fieldName]));
+          
         } else {
             // Otherwise put a 0 there
             $('input[id=venta_'+fieldName+']').val(0);
