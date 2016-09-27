21ad9a3b1189d20a52cc097511f7249691a9b8e0
Libu Librería <libulibreria@gmail.com>
2016-09-27 21:28:12 +0200

    Caja mensual funcional

--- src/Trinity/LibuBundle/Controller/EmailController.php
+++ src/Trinity/LibuBundle/Controller/EmailController.php
@@ -4,36 +4,9 @@ namespace Trinity\LibuBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
-use Trinity\LibuBundle\Form\VentaType;
-use Trinity\LibuBundle\Form\TipoType;
-use Trinity\LibuBundle\Form\LibroType;
-use Trinity\LibuBundle\Form\LibroCortoType;
-use Trinity\LibuBundle\Form\BaldaType;
-use Trinity\LibuBundle\Form\ProductoType;
-use Trinity\LibuBundle\Form\ResponsableType;
-use Trinity\LibuBundle\Form\ClienteType;
-use Trinity\LibuBundle\Form\TematicaType;
-use Trinity\LibuBundle\Form\FacturarType;
-use Trinity\LibuBundle\Form\MenuType;
-use Trinity\LibuBundle\Entity\Venta;
-use Trinity\LibuBundle\Entity\Cliente;
-use Trinity\LibuBundle\Entity\Responsable;
-use Trinity\LibuBundle\Entity\Tematica;
-use Trinity\LibuBundle\Entity\Producto;
-use Trinity\LibuBundle\Entity\ProductoVendido;
-use Trinity\LibuBundle\Entity\Libro;
-use Trinity\LibuBundle\Entity\Tipo;
-use Trinity\LibuBundle\Entity\VentaRepository;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\Form\Extension\Core\Type\DateTimeType;
 use Doctrine\Common\Collections\ArrayCollection;
-
-use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
-use Symfony\Component\Form\Extension\Core\Type\TextType;
-use Symfony\Component\Form\Extension\Core\Type\IntegerType;
-use Symfony\Component\Form\Extension\Core\Type\SubmitType;
-
 use Symfony\Component\Serializer\Serializer;
 use Symfony\Component\Serializer\Encoder\XmlEncoder;
 use Symfony\Component\Serializer\Encoder\JsonEncoder;
