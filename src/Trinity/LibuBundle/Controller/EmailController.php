5cec8be05bac307a2f862523f50ec48fae5d062e
Libu Librería <libulibreria@gmail.com>
2016-09-27 18:36:15 +0200

    Creado EmailController

---
+++ src/Trinity/LibuBundle/Controller/EmailController.php
@@ -0,0 +1,76 @@
+<?php
+
+namespace Trinity\LibuBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Trinity\LibuBundle\Form\VentaType;
+use Trinity\LibuBundle\Form\TipoType;
+use Trinity\LibuBundle\Form\LibroType;
+use Trinity\LibuBundle\Form\LibroCortoType;
+use Trinity\LibuBundle\Form\BaldaType;
+use Trinity\LibuBundle\Form\ProductoType;
+use Trinity\LibuBundle\Form\ResponsableType;
+use Trinity\LibuBundle\Form\ClienteType;
+use Trinity\LibuBundle\Form\TematicaType;
+use Trinity\LibuBundle\Form\FacturarType;
+use Trinity\LibuBundle\Form\MenuType;
+use Trinity\LibuBundle\Entity\Venta;
+use Trinity\LibuBundle\Entity\Cliente;
+use Trinity\LibuBundle\Entity\Responsable;
+use Trinity\LibuBundle\Entity\Tematica;
+use Trinity\LibuBundle\Entity\Producto;
+use Trinity\LibuBundle\Entity\ProductoVendido;
+use Trinity\LibuBundle\Entity\Libro;
+use Trinity\LibuBundle\Entity\Tipo;
+use Trinity\LibuBundle\Entity\VentaRepository;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Form\Extension\Core\Type\DateTimeType;
+use Doctrine\Common\Collections\ArrayCollection;
+
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\IntegerType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+
+use Symfony\Component\Serializer\Serializer;
+use Symfony\Component\Serializer\Encoder\XmlEncoder;
+use Symfony\Component\Serializer\Encoder\JsonEncoder;
+use Symfony\Component\Serializer\Normalizer\ObjectNormalizer;
+
+class EmailController extends Controller
+{
+     /**
+     * @Route("/libu/email", name="email")
+     */
+    public function emailAction(Request $request)
+    {
+
+        $em = $this->getDoctrine()->getManager();
+        $ventas = $em->getRepository('LibuBundle:Venta')->findAll();
+
+        $encoders = array(new XmlEncoder(), new JsonEncoder());
+        $normalizers = array(new ObjectNormalizer());
+
+        $serializer = new Serializer($normalizers, $encoders);
+        
+        $reports = $serializer->serialize($ventas, 'json');
+
+        $message = \Swift_Message::newInstance()
+            ->setSubject('Email de Libu')
+            ->setFrom('libulibreria@gmail.com')
+            ->setTo('libulibreria@gmail.com')
+            ->setBody(
+                $this->render('LibuBundle:libu:email.html.twig',array(
+                    'report' => $reports)
+                ),
+                'text/html'
+            )
+
+        ;
+        $this->get('mailer')->send($message);
+
+        return new Response('Correo enviado<br>');
+    }   
+}
--- src/Trinity/LibuBundle/Controller/LibuController.php
+++ src/Trinity/LibuBundle/Controller/LibuController.php
@@ -579,39 +579,5 @@ class LibuController extends Controller
     }
 
 
-     /**
-     * @Route("/libu/email", name="email")
-     */
-    public function emailAction(Request $request)
-    {
-
-        $em = $this->getDoctrine()->getManager();
-        $ventas = $em->getRepository('LibuBundle:Venta')->findAll();
-
-        $encoders = array(new XmlEncoder(), new JsonEncoder());
-        $normalizers = array(new ObjectNormalizer());
-
-        $serializer = new Serializer($normalizers, $encoders);
-        
-        $reports = $serializer->serialize($ventas, 'json');
-
-        $message = \Swift_Message::newInstance()
-            ->setSubject('Email de Libu')
-            ->setFrom('libulibreria@gmail.com')
-            ->setTo('libulibreria@gmail.com')
-            ->setBody(
-                $this->render('LibuBundle:libu:email.html.twig',array(
-                    'report' => $reports)
-                ),
-                'text/html'
-            )
-
-        ;
-        $this->get('mailer')->send($message);
-
-        return new Response('Correo enviado<br>');
-    }   
-
-
 
 }
