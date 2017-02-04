64686399f787682532f2d9ad7db1131e64666ae2
Borja <libulibreria@gmail.com>
2017-02-04 23:08:43 +0100

    Uso de bootstrap

--- src/Trinity/LibuBundle/Controller/BookController.php
+++ src/Trinity/LibuBundle/Controller/BookController.php
@@ -52,90 +52,25 @@ class BookController extends Controller
 
 //        echo "<pre>"; print_r($csv); echo "</pre>";
 
-        $columnas = array(
-            "ISBN" => 0,
-            "Título" => 1, 
-            "Autor" => 2, 
-            "Código" => 3    
-        );
 
-
-        echo "<h1>Libros encontrados:</h1>"; 
-        $i = 1;
+        $text = "<h1>Libros encontrados:</h1>"; 
+        $lista = array();
+        $i = 0;
         foreach ($csv as $book) {
             $isbn = filter_var($book[0], FILTER_SANITIZE_NUMBER_INT); 
-            if ($isbn == "") {
-                echo "<b>".$i++." - ISBN NO CORRECTO</b>";      
+            if ($isbn != "") {
+                $lista[$i][] = $i;
+                foreach ($book as $col) {
+                    $lista[$i] = $book;
+                }                
             } else {      
-                echo "<b>".$i++."</b>";
-                foreach ($columnas as $col => $key) {
-                    echo " - ".$col.": ".$book[$key];
-                }
+                if ($i != 0) $lista[$i] = array($i, "ISBN NO CORRECTO", "", "", "");   
             }
-            echo "<br>"; 
+            $i++; 
         }
 
-        // Crear un nuevo recurso cURL
-        $ch = curl_init();
-
-        // Adjunta el archivo xml
-        // $cfile = file_get_contents('/home/borja/peticion.xml');
-        // $data = array('peticion' => $cfile);
-/*
-        // Orden de conocer el pedido 132857690
-        $cfile = '
-        <?xml version="1.0" encoding="ISO-8859-1"?>
-        <orderUpdateRequest version="1.0">
-            <action name="getOrder">
-                <username>'.$csv[1][0].'</username>
-                <password>'.$csv[1][1].'</password>
-            </action>
-            <purchaseOrder id="132857690" />
-        </orderUpdateRequest>
-        ';
-*/
-        $abe_user = $this->getParameter('abebooks_user');
-        $abe_pass = $this->getParameter('abebooks_pass');        
-        // echo "Loader:<br><pre>"; print_r($loader); echo "</pre>";
-
-        $book = 1; 
-        $cfile = '
-        <?xml version="1.0" encoding="ISO-8859-1"?>
-        <inventoryUpdateRequest version="1.0">
-            <action name="bookupdate">
-                <username>'.$abe_user.'</username>
-                <password>'.$abe_pass.'</password>
-            </action>
-            <AbebookList>
-                <Abebook>
-                    <transactionType>add</transactionType>
-                    <vendorBookID>'.$csv[$book][3].'</vendorBookID>
-                    <author>'.$csv[$book][2].'</author>
-                    <title>'.$csv[$book][1].'</title>
-                    <publisher></publisher>
-                    <subject></subject>
-                    <price currency="EUR">'.$csv[$book][8].'</price>
-                    <dustJacket></dustJacket>
-                    <binding type="hard"></binding>
-                    <firstEdition>false</firstEdition>
-                    <signed>false</signed>
-                    <booksellerCatalogue></booksellerCatalogue>
-                    <description></description>
-                    <bookCondition>Fine</bookCondition>
-                    <size></size>
-                    <jacketCondition>Fine</jacketCondition>
-                    <bookType></bookType>
-                    <isbn>'.$csv[$book][0].'</isbn>
-                    <publishPlace></publishPlace>
-                    <publishYear></publishYear>
-                    <edition></edition>
-                    <inscriptionType></inscriptionType>
-                    <quantity amount="1"></quantity>
-                </Abebook>
-            </AbebookList>
-        </inventoryUpdateRequest>
-        ';
-
+        $tabla["contenido"] = $lista;
+        $tabla["cabecera_array"] = array("ISBN", "Título", "Autor", "Código");
 
         // Crea los botones para el formulario
         $form = $this->createFormBuilder()
@@ -143,31 +78,110 @@ class BookController extends Controller
             ->add('stop', SubmitType::class, array('label' => 'No subir'))            
             ->getForm();
 
+
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+
+            if ($form->get('continue')->isClicked()) {
+
+                // $datos = $form->getData();
+
+                // Crear un nuevo recurso cURL
+                $ch = curl_init();
+
+                // Adjunta el archivo xml
+                // $cfile = file_get_contents('/home/borja/peticion.xml');
+                // $data = array('peticion' => $cfile);
+        /*
+                // Orden de conocer el pedido 132857690
+                $cfile = '
+                <?xml version="1.0" encoding="ISO-8859-1"?>
+                <orderUpdateRequest version="1.0">
+                    <action name="getOrder">
+                        <username>'.$csv[1][0].'</username>
+                        <password>'.$csv[1][1].'</password>
+                    </action>
+                    <purchaseOrder id="132857690" />
+                </orderUpdateRequest>
+                ';
+        */
+    /*            $abe_user = $this->getParameter('abebooks_user');
+                $abe_pass = $this->getParameter('abebooks_pass');        
+                // echo "Loader:<br><pre>"; print_r($loader); echo "</pre>";
+
+                $book = 1; 
+                $cfile = '
+                <?xml version="1.0" encoding="ISO-8859-1"?>
+                <inventoryUpdateRequest version="1.0">
+                    <action name="bookupdate">
+                        <username>'.$abe_user.'</username>
+                        <password>'.$abe_pass.'</password>
+                    </action>
+                    <AbebookList>
+                        <Abebook>
+                            <transactionType>add</transactionType>
+                            <vendorBookID>'.$csv[$book][3].'</vendorBookID>
+                            <author>'.$csv[$book][2].'</author>
+                            <title>'.$csv[$book][1].'</title>
+                            <publisher></publisher>
+                            <subject></subject>
+                            <price currency="EUR">'.$csv[$book][8].'</price>
+                            <dustJacket></dustJacket>
+                            <binding type="hard"></binding>
+                            <firstEdition>false</firstEdition>
+                            <signed>false</signed>
+                            <booksellerCatalogue></booksellerCatalogue>
+                            <description></description>
+                            <bookCondition>Fine</bookCondition>
+                            <size></size>
+                            <jacketCondition>Fine</jacketCondition>
+                            <bookType></bookType>
+                            <isbn>'.$csv[$book][0].'</isbn>
+                            <publishPlace></publishPlace>
+                            <publishYear></publishYear>
+                            <edition></edition>
+                            <inscriptionType></inscriptionType>
+                            <quantity amount="1"></quantity>
+                        </Abebook>
+                    </AbebookList>
+                </inventoryUpdateRequest>
+                ';
+
+                // Establecer URL y otras opciones apropiadas
+        //        curl_setopt($ch, CURLOPT_URL, "https://orderupdate.abebooks.com:10003");
+                curl_setopt($ch, CURLOPT_URL, "https://inventoryupdate.abebooks.com:10027");        
+                curl_setopt($ch, CURLOPT_HEADER, "Content-Type: application/xml");
+                curl_setopt($ch, CURLOPT_POST, true);
+                curl_setopt($ch, CURLOPT_POSTFIELDS, $cfile);
+                curl_setopt($ch, CURLOPT_RETURNTRANSFER, true); 
+
+                // Capturar la URL y pasarla al navegador
+        //        $resultado = curl_exec($ch);
+
+                // Cerrar el recurso cURL y liberar recursos del sistema
+                curl_close($ch);
+    */ 
+    // echo "Resultado: <br>"; echo "<pre>"; print_r($resultado); echo "</pre>";
+
+            }
+
+            if ($form->get('stop')->isClicked()) {
+
+            }
+
+                return new Response ("ya está");
+ //           return $this->redirectToRoute('venta');
+        }
+
+
         return $this->render('LibuBundle:libu:simple.html.twig', array(
+ //           'lista' => $lista,
+            'texto_previo' => $text,
+            'tabla' => $tabla,
             'form' => $form->createView(),
         ));
-
-
-        // Establecer URL y otras opciones apropiadas
-//        curl_setopt($ch, CURLOPT_URL, "https://orderupdate.abebooks.com:10003");
-        curl_setopt($ch, CURLOPT_URL, "https://inventoryupdate.abebooks.com:10027");        
-        curl_setopt($ch, CURLOPT_HEADER, "Content-Type: application/xml");
-        curl_setopt($ch, CURLOPT_POST, true);
-        curl_setopt($ch, CURLOPT_POSTFIELDS, $cfile);
-        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true); 
-
-        // Capturar la URL y pasarla al navegador
-//        $resultado = curl_exec($ch);
-
-        // Cerrar el recurso cURL y liberar recursos del sistema
-        curl_close($ch);
- 
-// echo "Resultado: <br>"; echo "<pre>"; print_r($resultado); echo "</pre>";
-
-//        return new Response('<br>Finalizado');
-		return $this->render('LibuBundle:libu:simple.html.twig', array(
-			'form' => $form->createView(),
-			));    
+  
 	}
 }
 
