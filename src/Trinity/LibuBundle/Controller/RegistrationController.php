cf177f6548e538bbedea6ddbe3de97e7abced9fd
Sensibilizacion Zubietxe <sensibilizacion@zubietxe.org>
2017-02-16 16:24:56 +0100

    Instalado security, no funciona aún

---
+++ src/Trinity/LibuBundle/Controller/RegistrationController.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace Trinity\LibuBundle\Controller;
+
+use Trinity\LibuBundle\Form\UserType;
+use Trinity\LibuBundle\Entity\User;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
+
+class RegistrationController extends Controller
+{
+    /**
+     * @Route("/register", name="register")
+     */
+    public function registerAction(Request $request)
+    {
+        // Create a new blank user and process the form
+        $user = new User();
+        $form = $this->createForm(UserType::class, $user);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            // Encode the new users password
+            $encoder = $this->get('security.password_encoder');
+            $password = $encoder->encodePassword($user, $user->getPlainPassword());
+            $user->setPassword($password);
+
+            // Set their role
+            $user->setRole('ROLE_USER');
+
+            // Save
+            $em = $this->getDoctrine()->getManager();
+            $em->persist($user);
+            $em->flush();
+
+            return $this->redirectToRoute('login');
+        }
+
+        return $this->render('LibuBundle:libu:auth.html.twig', [
+            'form' => $form->createView(),
+        ]);
+    }
+}
