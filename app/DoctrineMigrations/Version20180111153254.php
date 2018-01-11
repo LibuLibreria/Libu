eb2d1aa741b9e9012d2663159822992c3c27a004
Sensibilizacion Zubietxe <sensibilizacion@zubietxe.org>
2018-01-11 16:52:53 +0100

    Cambiada columna Venta.factura a nuevo formato

---
+++ app/DoctrineMigrations/Version20180111153254.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace Application\Migrations;
+
+use Doctrine\DBAL\Migrations\AbstractMigration;
+use Doctrine\DBAL\Schema\Schema;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+class Version20180111153254 extends AbstractMigration
+{
+    /**
+     * @param Schema $schema
+     */
+    public function up(Schema $schema)
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
+
+        $this->addSql('ALTER TABLE venta CHANGE factura factura VARCHAR(20) DEFAULT NULL');
+        $this->addSql('UPDATE venta SET factura = CONCAT("L",LPAD(factura,7,"0"),"-",YEAR(diaHora))');
+    }
+
+    /**
+     * @param Schema $schema
+     */
+    public function down(Schema $schema)
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
+
+        $this->addSql('UPDATE venta SET factura = SUBSTRING(factura,2,7)');
+        $this->addSql('ALTER TABLE venta CHANGE factura factura INT(11) DEFAULT NULL COLLATE latin1_swedish_ci');
+    }
+}
